8b6e094ceb4660601149ef05578eae0fd489a7f8
czvr0 <chri656v@edu.sde.dk>
2019-02-24 13:28:30 +0100

    admins can now change default temperature

--- public_html/js/default-temperature-panel.js
+++ public_html/js/default-temperature-panel.js
@@ -1,11 +1,54 @@
 $(document).ready(function () {
 
-    $("#edit-default-temperature").click(function (e) {
+    getDefaultTemperature($("#temperature-input"));
+
+    $("#save-new-default-temperature").click(function (e) {
         e.preventDefault();
 
-        $("#edit-default-temperature-modal").modal("show");
+        const form = $($($($(this).parent()).parent()).parent());
 
-    })
+        setDefaultTemperature(form.serialize() + "&format=1", function (response) {
+            const output = $("#ui-box");
 
+            if(response.status) {
+                output.html(createDismissibleMessage("success", "Succes!", "Standard temperaturen er nu blevet sat for alle rum"));
+                getDefaultTemperature($("#temperature-input"));
+            }else {
+                output.html(createDismissibleMessage("danger", "Fejl!", "Du kan ikke sætte den samme standard temperatur igen!"));
+            }
+
+        });
+
+    });
+
+    function setDefaultTemperature(values, callback) {
+        $.ajax({
+            type: "POST",
+            url: apiURL() + "/api/temperature/setDefaultTemperature",
+            data: values,
+            headers: {"Authorization" : "Bearer " + getCookie("token")},
+            success: function(result){
+                callback(result);
+            },
+            error: function (result) {
+                callback(result);
+            }
+        });
+    }
+
+    function getDefaultTemperature(target) {
+        $.ajax({
+            type: "POST",
+            url: apiURL() + "/api/temperature/getDefaultTemperature",
+            data: "",
+            headers: {"Authorization" : "Bearer " + getCookie("token")},
+            success: function(result){
+                target.val(result.temperature);
+            },
+            error: function (result) {
+                console.log(result);
+            }
+        });
+    }
 
 });
